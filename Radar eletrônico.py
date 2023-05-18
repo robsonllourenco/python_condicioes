00d19447187cee16f8b1e87336e4217adf006399
Robson Lourenço <125693025+robsonllourenco@users.noreply.github.com>
2023-05-18 16:21:30 -0300

    Delete Radar eletrônico.py

--- Radar eletrônico.py
+++
@@ -1,9 +0,0 @@
-radar = float(input("Informe a velocidade do carro: "))
-
-multa = (radar-80) * 7
-
-if (radar>80):
-    print("VOCÊ ULTRAPASSOU A VELOCIDADE PERMITIDA")
-    print("Você foi multado no valor de R$ {:.2f}!".format(multa))
-else:
-    print("Você está na velocidade permitida.")
